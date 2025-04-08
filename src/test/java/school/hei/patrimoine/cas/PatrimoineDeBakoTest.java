ce18ed177be81bf1c7eac485cd247e64cce9a975
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2025-04-08 09:53:12 +0300

    update test cas patrimoine Bako

--- src/test/java/school/hei/patrimoine/cas/PatrimoineDeBakoTest.java
+++ src/test/java/school/hei/patrimoine/cas/PatrimoineDeBakoTest.java
@@ -6,21 +6,21 @@ import static school.hei.patrimoine.modele.Devise.*;
 import java.time.LocalDate;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import school.hei.patrimoine.cas.example.PatrimoineBako31Décembre2025;
+import school.hei.patrimoine.cas.example.PatrimoineBako;
 
 @Slf4j
 public class PatrimoineDeBakoTest {
 
     @Test
     void patrimoine_de_bako_au_31_decembre_2025() {
-        var patrimoineDeBako = new PatrimoineBako31Décembre2025().get();
+        var patrimoineBako = new PatrimoineBako().get();
 
-        var valeurPatrimoineProjete =
-                patrimoineDeBako
+        var patrimoineProjete =
+                patrimoineBako
                         .projectionFuture(LocalDate.of(2025, DECEMBER, 31))
                         .getValeurComptable(MGA)
                         .ppMontant();
 
-        log.error("{}", valeurPatrimoineProjete);
+        log.error("{}", patrimoineProjete);
     }
 }
