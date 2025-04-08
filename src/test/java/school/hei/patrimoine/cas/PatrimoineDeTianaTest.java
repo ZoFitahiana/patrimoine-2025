07a38ebc9500f8bba7aa0fb3f6127fa7b2424d9d
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2025-04-08 09:59:08 +0300

    Create test cas patrimoine Tiana

--- src/test/java/school/hei/patrimoine/cas/PatrimoineDeTianaTest.java
+++ src/test/java/school/hei/patrimoine/cas/PatrimoineDeTianaTest.java
@@ -7,20 +7,20 @@ import static school.hei.patrimoine.modele.Devise.MGA;
 import java.time.LocalDate;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import school.hei.patrimoine.cas.example.PatrimoineDeTiana;
+import school.hei.patrimoine.cas.example.PatrimoineTiana;
 
 @Slf4j
 public class PatrimoineDeTianaTest {
     @Test
     void patrimoine_de_Tiana_au_31_decembre_2025() {
-        var patrimoineDeTiana = new PatrimoineDeTiana().get();
+        var patrimoineDeTiana = new PatrimoineTiana().get();
 
-        var valeurPatrimoineProjete =
+        var patrimoineProjete =
                 patrimoineDeTiana
                         .projectionFuture(LocalDate.of(2026, MARCH, 31))
                         .getValeurComptable(MGA)
                         .ppMontant();
 
-        log.error("{}", valeurPatrimoineProjete);
+        log.error("{}", patrimoineProjete);
     }
 }
